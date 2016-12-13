e2b89ea8345226b7caddf1bfd86e6cd007d41dc8
shcho-isle <pavlo.plynko@gmail.com>
2016-12-13 09:13:18 +0200

    HW01 - map instead list in DAO

--- src/main/java/ru/javawebinar/topjava/dao/MealDaoMemory.java
+++ src/main/java/ru/javawebinar/topjava/dao/MealDaoMemory.java
@@ -4,17 +4,21 @@ import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MealDaoMemory implements MealDao {
-    private List<Meal> meals;
+    private Map<Integer, Meal> meals;
     private AtomicInteger count;
 
     public MealDaoMemory() {
         count = new AtomicInteger(0);
-        meals = new CopyOnWriteArrayList<>();
+        meals = new ConcurrentHashMap<>();
 
         add(new Meal(LocalDateTime.of(2215, Month.MAY, 30, 10, 0), "Завтрак", 500));
         add(new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000));
@@ -26,41 +30,35 @@ public class MealDaoMemory implements MealDao {
 
     @Override
     public void add(Meal meal) {
-        meal.setId(count.incrementAndGet());
-        meals.add(meal);
+        int id = count.incrementAndGet();
+        meal.setId(id);
+        meals.put(id, meal);
     }
 
     @Override
     public void delete(int id) {
-        for (Meal meal : meals)
-            if (meal.getId() == id) {
-                meals.remove(meal);
-                break;
-            }
+        if (meals.containsKey(id)) {
+            meals.remove(id);
+        }
     }
 
     @Override
     public void update(Meal meal) {
-        for (int i = 0; i < meals.size(); i++)
-            if (meals.get(i).getId() == meal.getId()) {
-                meals.set(i, meal);
-                return;
-            }
+        int id = meal.getId();
 
-        add(meal);
+        if (meals.containsKey(id))
+            meals.put(id, meal);
+        else
+            add(meal);
     }
 
     @Override
     public List<Meal> getAll() {
-        return meals;
+        return new ArrayList<>(meals.values());
     }
 
     @Override
     public Meal getById(int id) {
-        for (Meal meal : meals)
-            if (meal.getId() == id)
-                return meal;
-
-        return null;
+            return meals.getOrDefault(id, null);
     }
 }
