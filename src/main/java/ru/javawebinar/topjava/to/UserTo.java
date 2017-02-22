0691c44aacd403e41422f18167f20cc6a3a32caa
shcho-isle <pavlo.plynko@gmail.com>
2017-02-22 13:11:12 +0200

    10 06 profile jsptag

--- src/main/java/ru/javawebinar/topjava/to/UserTo.java
+++ src/main/java/ru/javawebinar/topjava/to/UserTo.java
@@ -84,6 +84,10 @@ public class UserTo implements HasId, Serializable {
         return caloriesPerDay;
     }
 
+    public void setCaloriesPerDay(Integer caloriesPerDay) {
+        this.caloriesPerDay = caloriesPerDay;
+    }
+
     @Override
     public String toString() {
         return "UserTo{" +
